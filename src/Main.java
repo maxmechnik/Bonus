7617ac467d35fd574875bf3a299b36c0f474cb04
maxmax <eparis@atlassian.com>
2023-02-13 23:56:53 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int deposit = 1200;
+
+        int bonus;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+        int totalAmount = account + deposit + bonus;
+
+
+        System.out.println("Итоговая сумма на счете " + totalAmount + "Р" + " в том числе BONUS ");
+
+
+    }
+}
